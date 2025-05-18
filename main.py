69eefdcb99ad8df36b70d7e9171106a0677dbc2a
JasonRTL <jasonrtlane@gmail.com>
2025-05-18 16:56:22 +1000

    Added clock and dt variable - Limited game loop to 60 fps

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +19,8 @@ def main():
             
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
     
