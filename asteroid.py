e365ec41ae8f5e1468f20112f2e2032163ca1f27
JasonRTL <jasonrtlane@gmail.com>
2025-05-21 17:09:02 +1000

    Astoids velocity fixed

--- asteroid.py
+++ asteroid.py
@@ -2,14 +2,11 @@ from circleshape import *
 
 class Astroid(CircleShape):
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
-        self.radius = radius
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * self.velocity * dt
+        self.position += self.velocity * dt
         
